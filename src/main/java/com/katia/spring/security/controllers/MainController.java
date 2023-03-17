0b424a3b363593a764bd7034789c993582c8142f
DeusIntra <deusintra@yahoo.com>
2023-03-17 21:26:05 +0500

    css startup

--- src/main/java/com/katia/spring/security/controllers/MainController.java
+++ src/main/java/com/katia/spring/security/controllers/MainController.java
@@ -1,7 +1,7 @@
 package com.katia.spring.security.controllers;
 
-import com.katia.spring.security.entities.User;
-import com.katia.spring.security.services.UserService;
+//import com.katia.spring.security.entities.User;
+//import com.katia.spring.security.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,49 +10,55 @@ import java.util.List;
 
 @Controller
 public class MainController {
-    private final UserService userService;
-    @Autowired
-    public MainController (UserService userService) {
-        this.userService = userService;
+
+    @GetMapping("/")
+    public String hello(Model model) {
+//        model.addAttribute("index", index);
+        return "index";
     }
-    @GetMapping("/users")
-    public String findAll(Model model){
-        List<User> users = userService.findAll();
-        model.addAttribute("users", users);
-        return "users";
-    }
-    @GetMapping("/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "show";
-    }@GetMapping("/new")
-    public String createUser(Model model) {
-        model.addAttribute("user", new User());
-        return "new";
-    }
-    @PostMapping("/users")
-    public String createUser(@ModelAttribute("user") User user, Model model){
-        User user1 = userService.saveUser(user);
-        model.addAttribute("user", user1);
-        return "redirect:/users";
-    }
-    @GetMapping("/{id}/edit")
-    public String updateUserForm(Model model, @PathVariable("id") Long id) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "edit";
-    }
-    @PostMapping("/{id}")
-    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
-        userService.updateUser(id, user);
-        return "redirect:/users";
-    }
-    @PostMapping("/users/{id}")
-    public String deleteUser(@PathVariable("id") Long id){
-        userService.deleteById(id);
-        return "redirect:/users";
-    }
+//    private final UserService userService;
+//    @Autowired
+//    public MainController (UserService userService) {
+//        this.userService = userService;
+//    }
+//    @GetMapping("/users")
+//    public String findAll(Model model){
+//        List<User> users = userService.findAll();
+//        model.addAttribute("users", users);
+//        return "users";
+//    }
+//    @GetMapping("/{id}")
+//    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+//        User user = userService.findById(id);
+//        model.addAttribute("user", user);
+//        return "show";
+//    }@GetMapping("/new")
+//    public String createUser(Model model) {
+//        model.addAttribute("user", new User());
+//        return "new";
+//    }
+//    @PostMapping("/users")
+//    public String createUser(@ModelAttribute("user") User user, Model model){
+//        User user1 = userService.saveUser(user);
+//        model.addAttribute("user", user1);
+//        return "redirect:/users";
+//    }
+//    @GetMapping("/{id}/edit")
+//    public String updateUserForm(Model model, @PathVariable("id") Long id) {
+//        User user = userService.findById(id);
+//        model.addAttribute("user", user);
+//        return "edit";
+//    }
+//    @PostMapping("/{id}")
+//    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+//        userService.updateUser(id, user);
+//        return "redirect:/users";
+//    }
+//    @PostMapping("/users/{id}")
+//    public String deleteUser(@PathVariable("id") Long id){
+//        userService.deleteById(id);
+//        return "redirect:/users";
+//    }
 
 
 }
--- src/main/java/com/katia/spring/security/repositories/UserRepository.java
+++ src/main/java/com/katia/spring/security/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.katia.spring.security.repositories;
+//
+//import com.katia.spring.security.entities.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.katia.spring.security.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
+//public interface UserRepository extends JpaRepository<User, Long> {
+//}
